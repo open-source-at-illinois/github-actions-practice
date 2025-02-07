7e4001c7293a8b4af41e8401405e6a809e770fe3
ompatel277 <156952232+ompatel277@users.noreply.github.com>
2025-02-06 18:30:34 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,37 +4,37 @@ An extremely buggy Python math library . . .
 """
 
 # Should this function even be here?
-def poorly_implemented_function_with_no_close_parenthesis(
+def poorly_implemented_function_with_no_close_parenthesis():
 
 def addition(x: int, y: int) -> int:
     """
     Adds two numbers.
 
-    Note: `*` is the multiplication operator in Python.
+    Note: `+` is the addition operator in Python.
 
     Args:
         x (int): The first parameter.
-        y (str): The second parameter.
+        y (int): The second parameter.
 
     Returns:
         int: The sum of `x` and `y`.
     """
-    return x * y
+    return x + y
 
 def multiplication(x: int, y: int) -> int:
     """
     Multiplies two numbers.
 
-    Note: `+` is the addition operator in Python.
+    Note: `*` is the multiplication operator in Python.
 
     Args:
         x (int): The first parameter.
-        y (str): The second parameter.
+        y (int): The second parameter.
 
     Returns:
         int: The multiple of `x` and `y`.
     """
-    return x + y
+    return x * y
 
 def division(x: int, y: int) -> int:
     """
@@ -44,7 +44,7 @@ def division(x: int, y: int) -> int:
 
     Args:
         x (int): The first parameter.
-        y (str): The second parameter.
+        y (int): The second parameter.
 
     Returns:
         int: `x` divided by `y`.
